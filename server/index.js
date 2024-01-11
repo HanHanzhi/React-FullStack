3188eb2aae91863676b57d2375dc336cf210920e
Hanzhi <Hanzhi100@outlook.com>
2024-01-11 18:31:18 +0800

    add comments

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 const db = require("./models");
 
 ////Routers
+////create endpoint "localhost:3001/post"
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
 
@@ -16,7 +17,7 @@ db.sequelize.sync().then(() => {
 });
 /*db.sequelize.sync() is a Sequelize method that synchronizes the 
 defined models with the database. It creates or updates database 
-tables based on the model definitions. 
+tables based on the model definitions. It go over all the table in the models folder. 
 This is a common step when setting up a new database or making changes 
 to the existing database schema. Note that the sync method should be 
 used with caution in production, as it can lead to data loss if not 
