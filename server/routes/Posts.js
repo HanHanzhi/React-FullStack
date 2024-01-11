57a7be4a96f098b80abd6524be67b17e7bfb121d
Hanzhi <Hanzhi100@outlook.com>
2024-01-11 17:24:42 +0800

    adding queries to db

---
+++ server/routes/Posts.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { Posts } = require("../models");
+/*The use of curly braces {} in the const { Posts } statement is known as object destructuring 
+in JavaScript. This syntax is commonly used when importing modules or requiring specific properties 
+from an object.*/
+
+/*When you use const { Posts } = require("../models");, it imports the object exported by the index.js 
+file in the models directory.The db object exported by index.js contains a property named Posts, which 
+represents the Sequelize model defined in Post.js. */
+
+router.get("/", async (req, res) => {
+  const listOfPosts = await Posts.findAll();
+  res.json(listOfPosts);
+});
+
+router.post("/123", async (req, res) => {
+  try {
+    //receive a FORM from frontend in JSON
+    const post = req.body;
+    //this body data should have the same format as our
+    //Post.js format in
+    await Posts.create(post);
+    res.json(post);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+module.exports = router;
