16dd6f07a84f16b8b2cfffc86d43254003751383
Hanzhi <Hanzhi100@outlook.com>
2024-01-12 16:14:29 +0800

    create a form in CreatePost.js with Formik

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 function CreatePost() {
-  return <div>CreatePost</div>;
+  return (
+    <div>
+      <Formik initialValues={0} onSubmit={0} validationSchema={0}>
+        <Form>
+          <label>Title: </label>
+          <Field id="inputCreatePost" name="title" placeholder="(EX..Title)" />
+          <label>Post: </label>
+          <Field
+            id="inputCreatePost"
+            name="postText"
+            placeholder="(EX..Post)"
+          />
+          <label>Username: </label>
+          <Field
+            id="inputCreatePost"
+            name="username"
+            placeholder="(EX..John123)"
+          />
+        </Form>
+      </Formik>
+    </div>
+  );
 }
 
 export default CreatePost;
