41fc33a214f6276b6e32d8e0a31845e4ced92e48
Hanzhi <Hanzhi100@outlook.com>
2024-01-12 15:04:05 +0800

    modify displayed information

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,13 @@ function App() {
   return (
     <div className="App">
       {listOfPosts.map((value, key) => {
-        return <div>{value.title}</div>;
+        return (
+          <div className="post">
+            <div className="title">{value.title}</div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
       })}
     </div>
   );
