36d43500e58c459d12c5447615b576357c214d5a
Hanzhi <Hanzhi100@outlook.com>
2024-01-18 21:24:44 +0800

    adding login routing on server

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -19,4 +19,19 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    //receive a FORM from frontend in JSON
+    const { username, password } = req.body; //{names} in bracket should match the property name in req.body
+    const user = await Users.findOne({ where: { username: username } });
+    if (!user) res.json({ error: "User Doesn't Exist" });
+    bcrypt.compare(password, user.password).then((match) => {
+      if (!match) return res.json({ error: "Wrong Username or Password" });
+      return res.json("Logged in Successfully");
+    });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 module.exports = router;
