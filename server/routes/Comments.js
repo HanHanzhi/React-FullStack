97d5b453b73e892848a83c93a5b00a940113b2d3
Hanzhi <Hanzhi100@outlook.com>
2024-01-19 16:51:02 +0800

    add middleware for backend routes

--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Comments } = require("../models");
+const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.get("/:postId123", async (req, res) => {
   const postId = req.params.postId123; //take note for req.params.name , name must be same as :name
@@ -8,7 +9,7 @@ router.get("/:postId123", async (req, res) => {
   return res.json(comments);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", validateToken, async (req, res) => {
   try {
     //receive a FORM from frontend in JSON
     const comment = req.body;
